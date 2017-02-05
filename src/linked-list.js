0573b356cebe276966fe7bf308154e7a52966ca2
Fedor <yakubovich.fedor@yandex.by>
2017-02-05 18:22:02 +0300

    Added constructor

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,7 +1,11 @@
 const Node = require('./node');
 
 class LinkedList {
-    constructor() {}
+  constructor() {
+    this.length = 0
+    this._tail = null
+    this._head = null
+  }
 
     append(data) {}
 
